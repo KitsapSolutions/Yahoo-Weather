4741f77a067ad5657cf8b624be8546d1754a287a
David Prentice <22718119+KitsapSolutions@users.noreply.github.com>
2019-06-23 20:07:46 -0700

    Initial commit
    Sanitized...

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+
+Yahoo Weather API configuration file
+
+"""
+import configparser
+config = configparser.ConfigParser()
+
+yahoo_app_id = 'Yahoo APP ID'
+yahoo_consumer_key = 'Yahoo Consumer Key'
+yahoo_consumer_secret = 'Yahoo Consumer Secret'
